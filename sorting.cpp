295a75907a26dc2066cb48d14285889303ac0347
hfaus <70163503+hfaus@users.noreply.github.com>
2022-01-18 08:59:20 -0500

    basic for sorting, started

---
+++ sorting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[]) {
+    for(int i=0; i < arr.length(); i++) {
+        std::cout << "Wow" << std::endl;
+    }
+}
+
+int main {
+    std::cout << "Begin sorting: " << std::endl;
+
+    int arr[] = {10, 5, 6, 9, 11, 3, 4};
+}
